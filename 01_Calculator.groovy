8c8fe25cfeb4b49e5d6f2e9d59fc2dcfadc1990d
HS <none@email.none>
2015-10-12 12:53:55 +0100

    .

---
+++ 01_Calculator.groovy
@@ -0,0 +1,14 @@
+print "Please enter a number "
+String str = System.console().readLine();
+int number1 = Integer.parseInt(str);
+
+print "Please enter another number "
+String str = System.console().readLine();
+int number2 = Integer.parseInt(str);
+
+println "Please an operation: "
+println "(1) Add (2) Subtract (3) Multiply (4) Divide"
+print "Your choice: "
+String str = System.console().readLine();
+int operation = Integer.parseInt(str);
+
