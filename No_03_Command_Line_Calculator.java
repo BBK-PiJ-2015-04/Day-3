fc96e528558813d7a479aac826fabed37d8d1d05
HS <none@email.none>
2015-10-13 14:11:44 +0100

    .

--- No_03_Command_Line_Calculator.java
+++ No_03_Command_Line_Calculator.java
@@ -5,17 +5,17 @@ public class No_03_Command_Line_Calculator {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String str;
+        String userInput;
         System.out.print ("Please enter command:  ");
-        str = System.console().readLine();
+        userInput = System.console().readLine();
         int length5;
         int i;
         int operator_location = -1;
-        length5 = str.length();
+        length5 = userInput.length();
         System.out.println (length5);
-        System.out.println (str.substring(0,1));
+        System.out.println (userInput.substring(0,1));
         for (i = 0; i <= (length5 - 1); i++) {
-            if(str.substring(i, i + 1) == "+") {
+            if(userInput.substring(i, i + 1) == "+") {
                 operator_location = i;
                 System.out.println(operator_location);
             }
--- No_04_Distance.java
+++ No_04_Distance.java
@@ -6,7 +6,6 @@ public class No_04_Distance {
 
     public static void main(String[] args) {
         String str;
-        double distance12, distance13, distance23;
         Point one = new Point();
         System.out.println ("Please enter the first co-ordinate...");
         System.out.print ("The X value: ");
@@ -34,19 +33,22 @@ public class No_04_Distance {
         str = System.console().readLine();
         three.y = Double.parseDouble(str);
         System.out.println();
-        Distance one = new Distance();
-        System.out.println(one.x);
-        System.out.println(one.y);
-        System.out.println(two.x);
-        System.out.println(two.y);
-        System.out.println(three.x);
-        System.out.println(three.y);
+        double distance12, distance13, distance23;
+        distance12 = Math.sqrt(((one.x - two.x) * (one.x - two.x)) + ((one.y - two.y) * (one.y - two.y)));
+        distance13 = Math.sqrt(((one.x - three.x) * (one.x - three.x)) + ((one.y - three.y) * (one.y - three.y)));
+        distance23 = Math.sqrt(((two.x - three.x) * (two.x - three.x)) + ((two.y - three.y) * (two.y - three.y)));
+        if (distance12 < distance13 && distance12 < distance23) {
+            System.out.println ("The Points 1 & 2 are the closest.");
+        }
+else if (distance13 < distance23) {
+            System.out.println ("The Points 1 & 3 are the closest.");
+        }
+else {
+            System.out.println ("The Points 2 & 3 are the closest.");
+        }
     }
     static class Point {
         double x;
         double y;
-    }
-    static class Distance {
-        double z;
     }
 }
